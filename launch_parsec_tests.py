5f75c23d746c7a3688679e5912be0d5d2673ab81
Kelly Nguyen <klynguyen@ucdavis.edu>
2021-01-16 00:05:14 -0800

    run

--- launch_parsec_tests.py
+++ launch_parsec_tests.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
 
     def createRun(bench, size, cpu, cores):
         return gem5Run.createFSRun(
-            'parsec_tutorial_run',
+            'parsec_tutorial',
             'gem5/build/X86/gem5.opt',
             'configs-parsec-tests/run_parsec.py',
             f'''results/{cpu}/{cores}/{bench}/{size}''',
